e1fdc5d05960a4ee9be51e1c85f54a5858d5439d
Guilherme Pinho <100877596+guillepinho@users.noreply.github.com>
2022-11-29 21:59:35 -0300

    🚑

--- TratamentoExcecao/src/CheckedException/Main.java
+++ TratamentoExcecao/src/CheckedException/Main.java
@@ -1,2 +1,31 @@
-package CheckedException;public class Main {
+package CheckedException;
+
+import javax.swing.*;
+import java.io.*;
+
+// Imprimir um arquivo no console
+public class Main {
+    public static void main(String[] args) {
+        String nomeDoArquivo = "romances-blake-crouch.txt";
+        imprimirArquivoNoConsole(nomeDoArquivo);
+
+        System.out.println("Apesar da exception ou não, o programa continua...");
+    }
+
+    static void imprimirArquivoNoConsole(String nomeDoArquivo) throws FileNotFoundException {
+        File file = new File(nomeDoArquivo);
+
+        BufferedReader br = new BufferedReader(new FileReader((file.getName())));
+        String line = br.readLine();
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        do {
+            bw.write(line);
+            bw.newLine();
+            line=br.readLine();
+        } while(line != null);
+        bw.flush();
+        br.close();
+    }
 }
--- TratamentoExcecao/src/UncheckedException/Main.java
+++ TratamentoExcecao/src/UncheckedException/Main.java
@@ -1,5 +1,31 @@
+package UncheckedException;
+
+import javax.swing.*;
+
 public class Main {
+    // Fazendo a divisão de dois valores inteiros
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean continueLooping = true;
+        do {
+            String a = JOptionPane.showInputDialog("Numerador: ");
+            String b = JOptionPane.showInputDialog("Denominador: ");
+            try {
+                int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
+                System.out.println("Resultado: " + resultado);
+                continueLooping = false;
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Entrada inválida, informe um número! " + e.getMessage());
+              throw new RuntimeException(e);
+            } catch (ArithmeticException e) {
+                JOptionPane.showMessageDialog(null, "Impossível dividir por zero! " + e.getMessage());
+            }
+            finally {
+                System.out.println("Chegou no finally!");
+            }
+        } while (continueLooping);
+
+        System.out.println("Chegou até aqui...");
     }
+
+    static int dividir(int a, int b) { return a / b; }
 }
